3cc6c444b12e9ab7c1017d095f710e7fa4b23787
yjglab <yjgdesign@gmail.com>
2022-03-25 02:26:56 +0900

    interface

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-console.log("compiled");
+interface Person {
+  name: string;
+  age: number;
+  gender: string;
+}
+
 const name = "yjglab",
   age = 100,
   gender = "male";
 
+const obj = {
+  name: "yjg",
+  age: 100,
+  gender: "male",
+};
 const say = (name: string, age: number, gender: string): boolean => {
-  console.log(`${name} ddssssssssaaa ${age} ${gender}`);
+  console.log(`say() => ${name} ddssssssssaaa ${age} ${gender}`);
 
   return true;
 };
+const sayObj = (obj: Person): string => {
+  return `sayObj() => ${obj.name} ${obj.age} ${obj.gender}`;
+};
 
 console.log(say(name, age, gender));
-
+console.log(sayObj(obj));
 export {};
